f22603acd4e31656504b3e691eea474fa8ffa856
know4avi <69908168+know4avi@users.noreply.github.com>
2022-04-22 12:58:15 +0530

    connection

--- src/main/java/com/capgemini/springboot/demo/controller/AppUserController.java
+++ src/main/java/com/capgemini/springboot/demo/controller/AppUserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.capgemini.springboot.demo.service.AppUserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class AppUserController implements IAppUserController {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
